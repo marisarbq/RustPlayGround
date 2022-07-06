d137a442483b3eb2e74550983c1c1bbe956c7626
Marisa <12@450.moe>
2022-07-06 13:09:36 +0800

    feat: 新增了一个web Server子仓库，用来示范rust web开发能力

--- README.md
+++ README.md
@@ -6,8 +6,15 @@ Learn Rust, Take The Future
 
 > I use CodeServer or VsCode 
 
-#### Plugins
+#### Vsix Plugins
 
-- rust-analyzer @v0.2.853 **Important!**
+Tools
+- rust-analyzer @v0.4.1116 **Important!**
 - Native Debug @v0.26.0
 - Better TOML @v0.3.2
+- Error Lens @v3.5.1
+- CodeLLDB @v1.7.0
+
+Theme
+- One Dark Pro
+
--- crates/webserver/src/main.rs
+++ crates/webserver/src/main.rs
@@ -1,3 +1,88 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::{Path, Query},
+    response::Html,
+    routing::{get, post},
+    Json, Router, Server,
+};
+use serde_json::{json, Value};
+use std::collections::HashMap;
+
+//因为我的IDE开发环境是在云端Docker容器，端口映射有一定规则， 容器 300x 端口会映射成外网1000x
+static HOST: &str = "39.103.223.49";
+static PORT_INDEX: &str = "9";
+
+// #[] 是Rust attr属性，属性是作用在 Rust 语言元素上的元数据。
+// https://cloud.tencent.com/developer/article/1594094
+#[tokio::main]
+async fn main() {
+    // run it
+    let true_host = format!("0.0.0.0:300{}", PORT_INDEX);
+    let addr = true_host.parse().unwrap();
+    println!(
+        "[服务器已启动]
+        地址:  {}
+        公网:  {}:1000{}",
+        addr, HOST, PORT_INDEX
+    );
+    Server::bind(&addr)
+        .serve(app().into_make_service())
+        .await
+        .unwrap();
+}
+
+fn app() -> Router {
+    Router::new()
+        .route("/", get(|| async { "服务器在线!" }))
+        .route("/user/:id", get(app_user))
+        .route("/get", get(app_get))
+        .route("/post", post(app_post))
+}
+
+//https://docs.rs/axum/latest/axum/#extractors
+async fn app_user(
+    Query(query): Query<HashMap<String, String>>, //query 和其他语言的web框架是一样的。
+    Path(path): Path<String>,                     //获取url路径，也一样
+    Json(payload): Json<Value>,
+) -> Html<&'static str> {
+    print!(
+        "
+    query: {:#?}
+    path: {:#?}
+    payload: {:#?}
+    ",
+        query,
+        path,
+        json!(payload)
+    );
+    Html("<h1>Hello, World!</h1>")
+}
+
+async fn app_get(
+    Query(query): Query<HashMap<String, String>>, //query 和其他语言的web框架是一样的。
+) -> Json<Value> {
+    Json(json!({
+        "code": 200,
+        "success": true,
+        "msg": "ok",
+        "data": {
+            "server_status": "online",
+            "ip": HOST,
+            "port_index": PORT_INDEX,
+            "query": query
+        }
+    }))
+}
+
+async fn app_post(Json(payload): Json<Value>) -> Json<Value> {
+    Json(json!({
+        "code": 200,
+        "success": true,
+        "msg": "ok",
+        "data": {
+            "server_status": "online",
+            "ip": HOST,
+            "port_index": PORT_INDEX,
+            "payload": payload
+        }
+    }))
 }
